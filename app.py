7ba7a8c1ee72026c3f6de45b81259cc6d33eb1d8
AwsSG <aws.sabah.gheni@gmail.com>
2022-05-01 21:48:55 +0000

    added function to check and count increase/decrease

--- app.py
+++ app.py
@@ -26,12 +26,26 @@ url = 'http://api.exchangeratesapi.io/v1/latest?access_key=1896b2be48dacf88b405e
 # TRY=data["rates"]["TRY"]
 # print(USD)
 # print(TRY)
-random_list = []
+random_list = [10]
+initial_capital = 100
+
 
 def random_number():
     """ dummy function to generate random numbers """
-    rndm = random.randint(0, 100)
+    decreas_count = 0
+    increas_count = 0
+    rndm = random.randint(-100, 100)
     random_list.append(rndm)
+    for count, ele in enumerate(random_list):
+        if count != 0:
+            if ele < (count - 1):
+                decreas_count += 1
+                increas_count = 0
+            elif ele > (count - 1):
+                increas_count += 1
+                decreas_count = 0
+    print(decreas_count)
+    print(increas_count)
     return random_list
 
 
@@ -39,9 +53,11 @@ def random_number():
 def home():
     """ the main view of the app """
     threading.Timer(10, random_number())
-    total_sum = sum(random_list)
 
-    return render_template("main.html", random_list=random_list, total_sum=total_sum)
+
+    # total_sum = sum(random_list)
+    return render_template("main.html", random_list=random_list,
+                           )
 
 
 if __name__ == "__main__":
