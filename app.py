aa50331374aad82b9e7fe22e01cd58c8e8d282f4
AwsSG <aws.sabah.gheni@gmail.com>
2022-05-04 16:18:31 +0000

    added trend shift to function

--- app.py
+++ app.py
@@ -34,30 +34,52 @@ def random_number():
     """ dummy function to generate random numbers """
     decreas_count = 0
     increas_count = 0
+    shift_up = 0
+    shift_down = 0
     rndm = random.randint(-100, 100)
     random_list.append(rndm)
     for count, ele in enumerate(random_list):
         if count != 0:
             if ele < (count - 1):
                 decreas_count += 1
+                if increas_count > 3:
+                    shift_down = 1
+                shift_up = 0
                 increas_count = 0
             elif ele > (count - 1):
                 increas_count += 1
+                if decreas_count > 3:
+                    shift_up = 1
+                shift_down = 0
                 decreas_count = 0
-    print(decreas_count)
-    print(increas_count)
+
+    if decreas_count > 3:
+        print("down trend")
+    if increas_count > 3:
+        print("up trend")
+    print("Decrease " + str(decreas_count))
+    print("Increase " + str(increas_count))
+    print("shift up = " + str(shift_up))
+    print("shift down = " + str(shift_down))
+    if shift_up > 0:
+        shift_up = 0
+        print("shift up")
+    if shift_down > 0:
+        shift_down = 0
+        print("shift down")
+
     return random_list
 
 
 @app.route("/")
 def home():
     """ the main view of the app """
-    threading.Timer(10, random_number())
-
+    threading.Timer(3, random_number())
+    capital = initial_capital
 
     # total_sum = sum(random_list)
     return render_template("main.html", random_list=random_list,
-                           )
+                           capital=capital)
 
 
 if __name__ == "__main__":
